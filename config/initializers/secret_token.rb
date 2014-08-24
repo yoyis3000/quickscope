03810d2f684cbe4232c82115bcf08866d78b85b2
george <georgebarrios@umail.ucsb.edu>
2014-08-23 19:03:08 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quickscope::Application.config.secret_token = '3a94a3e0cd443f99a0eaf074e8d6a2f73d3125298a09156f3579cd1c4094d7b955f33c709673d42174ab27c568455951050252e55e4396d822b7590a81557eaf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Quickscope::Application.config.session_store :cookie_store, key: '_quickscope_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Quickscope::Application.config.session_store :active_record_store
